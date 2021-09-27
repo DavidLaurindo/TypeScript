a1ab34f2770fae7d6d7ea8037876549e162c4d7d
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-09-27 15:59:39 -0300

    interfaces aula 3

--- interfaces/interfaces.js
+++ interfaces/interfaces.js
@@ -7,10 +7,15 @@ function mudarNome(pessoa) {
 }
 var pessoa = {
     nome: 'joão',
-    idade: 27
+    idade: 27,
+    saudar: function (sobrenome) {
+        console.log('Olá, meu nome é '
+            + this.nome + ' ' + sobrenome);
+    }
 };
 saudarComOla(pessoa);
 mudarNome(pessoa);
 saudarComOla(pessoa);
 //saudarComOla({nome: 'Jonas', idade: 27})//erro porque o atributo idade não está na interface Humano.
-saudarComOla({ nome: 'Cleitinho', idade: 27, qualquerNome: true });
+//saudarComOla({nome: 'Cleitinho', idade: 27, qualquerNome: true});
+pessoa.saudar('Skywalker');
--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -1,7 +1,11 @@
 interface Humano{
     nome: string
+
     idade?: number//a "?" quer dizer que é um atributo opicional. não é obrigatório ter esse atributo.
+
     [prop: string]: any//quando não sabe qual o nome do atributo([prop]) e quando não sabe qual o tipo(any). [pop: string] a string significa que o nome passado será em string(não se nomeia atributos com outro tipo.)
+
+    saudar(sobrenome: string):void;
 }
 
 function saudarComOla(pessoa: Humano){
@@ -12,13 +16,18 @@ function mudarNome(pessoa: Humano){
     pessoa.nome = 'joana';
 }
 
-const pessoa = {
+const pessoa: Humano = {
     nome: 'joão',
-    idade: 27
+    idade: 27,
+    saudar(sobrenome: string){
+        console.log('Olá, meu nome é '
+        + this.nome + ' ' + sobrenome)
+    }
 }
 saudarComOla(pessoa);
 mudarNome(pessoa);
 saudarComOla(pessoa);
 //saudarComOla({nome: 'Jonas', idade: 27})//erro porque o atributo idade não está na interface Humano.
 
-saudarComOla({nome: 'Cleitinho', idade: 27, qualquerNome: true});
+//saudarComOla({nome: 'Cleitinho', idade: 27, qualquerNome: true});
+pessoa.saudar('Skywalker');
